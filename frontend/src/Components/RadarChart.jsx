642a769717427bbf8bc0e392e81d2847523d7a3b
sagepages <anthonypages009@gmail.com>
2022-03-09 21:08:35 -0500

    basic setup - state setup - chart built

---
+++ frontend/src/Components/RadarChart.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Radar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+
+ChartJS.register(
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend
+);
+
+const RadarChart = ({ chartData }) => {
+  const options = {
+    scales: {
+      r: {
+        suggestedMin: 0,
+        suggestedMax: 100,
+      },
+    },
+  };
+  return <Radar data={chartData} options={options} />;
+};
+
+export default RadarChart;
--- frontend/src/Components/index.js
+++ frontend/src/Components/index.js
@@ -1,1 +1,2 @@
 export {default as Stage} from './Stage'
+export {default as RadarChart} from './RadarChart'
